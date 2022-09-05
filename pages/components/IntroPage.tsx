79e48de6cb15c6e9aa72152c9f75c17ce7fc71f3
waterbeen0530 <hange253@gmail.com>
2022-09-05 14:47:04 +0900

    [CREATE] IntroPage

---
+++ pages/components/IntroPage.tsx
@@ -0,0 +1,80 @@
+import styled from "@emotion/styled";
+
+export default function IntroPage() {
+  return (
+    <>
+      <Container id="intro">
+        <img src="/img/Group1.png" alt="" id="img1" />
+        <img src="/img/Group2.png" alt="" id="img2" />
+        <Wrapper>
+          <div>
+            <h2>Movie</h2>
+            <h2>recommendation</h2>
+            <p>Get recommendations from various movies!</p>
+          </div>
+          <img src="/img/MovieMan.png" alt="" />
+        </Wrapper>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  position: relative;
+  height: 90vh;
+  padding-top: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  /* overflow: hidden; */
+  background: #0e4c75;
+
+  #img1 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 20%;
+    height: auto;
+  }
+
+  #img2 {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 30%;
+    height: auto;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 1070px;
+  padding: 0 0 170px 20px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+
+  div {
+    padding: 20px 0 0 167px;
+  }
+
+  h2 {
+    margin: 0;
+    padding: 0 0;
+    font-size: 70px;
+    color: #fff;
+  }
+
+  p {
+    margin: 8px 0;
+    font-size: 30px;
+    color: #fff;
+  }
+
+  img {
+    position: absolute;
+    top: 180px;
+    right: 200px;
+    width: 30%;
+    height: auto;
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
+import styled from "@emotion/styled";
 import type { NextPage } from "next";
 import Header from "./components/common/Header";
+import IntroPage from "./components/IntroPage";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <Container>
       <Header />
-    </div>
+      <IntroPage />
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  /* background-color: red;
+  @media (max-width: 600px) {
+    background-color: blue;
+  } */
+`;
